1d42882726137471a75fe2ee6e95b33bfcd03f6f
zpice <zp2016@mail.dlut.edu.cn>
2018-07-16 16:15:55 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+import com.zp.client.Client;
+import com.zp.server.Server;
+
+
+
+public class Main {
+    public static void main(String[] args){
+        if(args[0].equals("client")){
+            Client client = new Client();
+            client.start();
+        }else {
+            Server app = new Server();
+            app.start();
+        }
+
+    }
+}
---
+++ src/com/zp/lib/RSADecrypt.java
@@ -0,0 +1,16 @@
+package com.zp.lib;
+
+public interface RSADecrypt {
+    byte[] publicKeyDecrypt(byte[] data);
+    byte[] privateKeyDecrypt(byte[] data);
+}
+
+//package cn.liuchaorun.lib;
+//
+//import java.io.InputStream;
+//import javax.crypto.CipherInputStream;
+//
+//public interface RSADecrypt {
+//    CipherInputStream publicKeyDecrypt(InputStream inputStream);
+//    CipherInputStream privateKeyDecrypt(InputStream inputStream);
+//}
